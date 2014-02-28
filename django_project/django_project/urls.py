c5193e695cbdcdbe0f90669a6bbe418c5cf0591d
Viktor Matys <v.matys@seznam.cz>
2014-02-28 11:25:57 +0100

    project's urls.py - add omitted tools for authentication

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
+import django.contrib.auth.views
+
 
 urlpatterns = patterns('',
     url(r'^work_evid/', include('work_evid.urls', namespace='work_evid')),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout'),
 )
 
